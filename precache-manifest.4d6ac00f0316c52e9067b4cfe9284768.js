49e85d1d875de8f7caf0b9016a64494ba19c2eec
Rob Thijssen <rthijssen@gmail.com>
2020-04-02 11:28:08 +0300

    Updates

---
+++ precache-manifest.4d6ac00f0316c52e9067b4cfe9284768.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bed85a78172c444a11dbec059fa8ecb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94e8b6c8ed7eb4ba77e6",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bf3b1d605d7c66ed8772",
+    "url": "/static/js/2.dc94f4db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dc94f4db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94e8b6c8ed7eb4ba77e6",
+    "url": "/static/js/main.17b68f8b.chunk.js"
+  },
+  {
+    "revision": "5ac652c2f3630c12db22",
+    "url": "/static/js/runtime-main.0c4f7dcf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
